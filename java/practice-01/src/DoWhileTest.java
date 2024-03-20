f2a574bb262e2c025d0d2384bc762099a9b0bbd0
typhoon0678 <typhoon0678@gmail.com>
2024-03-20 17:51:48 +0900

    java practice2

---
+++ java/practice-01/src/DoWhileTest.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class DoWhileTest {
+
+    public static void main(String[] args) {
+        for (int i=97; i<97+26; i++) {
+            System.out.print((char) i);
+        }
+    }
+}
